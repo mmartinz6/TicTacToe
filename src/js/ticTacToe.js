76bd17fc90938031301af4667fab45f18f669403
mmartinz6 <madmartinez@gmail.com>
2025-08-19 15:13:39 -0600

    Js funcionamiento mostrar jugador

--- src/js/ticTacToe.js
+++ src/js/ticTacToe.js
@@ -1,7 +1,50 @@
+const btnReiniciar = document.getElementById("btnReiniciar")
+const mensajeGanador = document.getElementById("mensajeGanador")
+const mensajeTurnoJugador = document.getElementById("mensajeTurnoJugador")
+
 const celdas = document.getElementsByClassName("celda");
 
 let turno = true
 
+//Lista de combinaciones posibles para ganar
+const combinacionesGanadoras = [
+    [0, 1, 2], //h
+    [3, 4, 5], //h
+    [6, 7, 8], //h
+    [0, 3, 6], //v
+    [1, 4, 7], //v
+    [2, 5, 8], //v
+    [0, 4, 8], //d
+    [2, 4, 6] //d
+]
+
+//Función para mostrar en pantalla el turno de cada jugador
+actualizarMensajeTurno()
+function actualizarMensajeTurno() {
+    if (turno) {
+        mensajeTurnoJugador.textContent = "JUGADOR 1"
+    }else{
+        mensajeTurnoJugador.textContent = "JUGADOR 2"
+    }
+}
+
+//Función para verificar ganador
+function verificarGanador() {
+    for (let index = 0; index < combinacionesGanadoras.length; index++) {
+      const [a, b, c,]  = combinacionesGanadoras[index]
+
+      if (
+        celdas[a].textContent !== "" &&
+        celdas[a].textContent === celdas[b].textContent &&
+        celdas[a].textContent === celdas[c].textContent
+        ){
+            return celdas[a].textContent;
+        }
+    }
+}
+
+
+//
 for (let index = 0; index < celdas.length; index++) {
     const element = celdas[index];
     console.log(index);
@@ -19,20 +62,26 @@ for (let index = 0; index < celdas.length; index++) {
 
             element.textContent = jugadorActual;
 
+            const ganador = verificarGanador();
+            if (ganador) {
+                mensajeGanador.textContent = "GANO EL JUGADOR",ganador
+                return;
+            }
             turno = !turno;
+
+            actualizarMensajeTurno()
         }
     })
 }
 
-const btnReiniciar = document.getElementById("btnReiniciar")
-const mensajeGanador = document.getElementById("mensajeGanador")
 
+//Botón reinicia el tablero del juego
 btnReiniciar.addEventListener("click", function () {
     for (let index = 0; index < celdas.length; index++) {
         celdas[index].textContent = "";
-        
     }
 
     mensajeGanador.textContent = ""
     turno = true
+    actualizarMensajeTurno()
 })
